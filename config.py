ade6cabf818d74c73d949b0e121569fba5baf963
telegrambet <guilhermer6futebol@gmail.com>
2025-07-17 06:57:04 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# Pares monitorados
+PAIRS = ['EURUSD', 'EURJPY', 'EURGBP', 'GBPJPY', 'USDJPY']
+
+# Timeframes usados nos indicadores
+INDICATORS_TIMEFRAMES = ['4h', '1h', '15m', '5m']
+CONFIRMATION_TIMEFRAME = '5m'
