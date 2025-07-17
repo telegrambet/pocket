924341d5015d7d7a9a0a621d43c58d0c38791cd3
telegrambet <guilhermer6futebol@gmail.com>
2025-07-17 07:32:56 -0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,22 +1,17 @@
+# telegram_bot.py
 import os
-import requests
+import telegram
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 def enviar_mensagem(texto):
-    if not TELEGRAM_TOKEN or not CHAT_ID:
-        print("Erro: Variáveis TELEGRAM_TOKEN ou TELEGRAM_CHAT_ID não configuradas.")
-        return
-
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": texto,
-        "parse_mode": "HTML"
-    }
     try:
-        response = requests.post(url, json=payload)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=texto)
+    except Exception as e:
         print(f"Erro ao enviar mensagem: {e}")
