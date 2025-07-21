6816e1d52361c04a13dc9ae3e09f245db8e1527e
telegrambet <guilhermer6futebol@gmail.com>
2025-07-21 15:54:38 -0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# Pares monitorados
-PAIRS = ['EURUSD', 'EURJPY', 'EURGBP', 'GBPJPY', 'USDJPY']
-
-# Timeframes usados nos indicadores
-INDICATORS_TIMEFRAMES = ['1h', '15m', '5m']
-CONFIRMATION_TIMEFRAME = '5m'
