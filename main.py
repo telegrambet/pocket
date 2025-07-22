0ec1b52b49dba359c22683b10be51ccd56908ffa
telegrambet <guilhermer6futebol@gmail.com>
2025-07-21 22:36:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-from telegram.ext import ApplicationBuilder
-from bot.commands import get_handlers
-from bot.scheduler import start_schedulers
-import os
+import logging
 from dotenv import load_dotenv
+from telegram.ext import ApplicationBuilder
+from bot.handlers import setup_handlers
 
 load_dotenv()
+
+import os
+
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-def start_bot():
-    app = ApplicationBuilder().token(TOKEN).build()
+logging.basicConfig(level=logging.INFO)
+app = ApplicationBuilder().token(TOKEN).build()
+setup_handlers(app)
 
-    for handler in get_handlers():
-        app.add_handler(handler)
-
-    # Iniciar agendamentos automáticos
-    start_schedulers(app)
-
-    print("✅ Bot rodando...")
+if __name__ == "__main__":
     app.run_polling()
-
-if __name__ == '__main__':
-    start_bot()
