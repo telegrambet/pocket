3ddc60afb306f13c7d3b0473318d41b7789c1d0d
telegrambet <guilhermer6futebol@gmail.com>
2025-07-25 12:47:27 -0300

    Create utilis.py

---
+++ utilis.py
@@ -0,0 +1,12 @@
+# pocket/utils.py
+
+def calcular_variacao(open_price, close_price):
+    return round((float(close_price) - float(open_price)) * 10000, 1)
+
+def interpretar_movimento(variacao_pips):
+    if variacao_pips >= 20:
+        return "Alta", f"+{variacao_pips} pips"
+    elif variacao_pips <= -20:
+        return "Baixa", f"{variacao_pips} pips"
+    else:
+        return None, None
