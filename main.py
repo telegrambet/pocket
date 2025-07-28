e60ba56306a85d2da5414c515a553f4c657a49c8
telegrambet <guilhermer6futebol@gmail.com>
2025-07-27 22:39:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# main.py
-import time
 import asyncio
 from utils import PAIRS
 from deriv import get_candles
-from telegram_bot import send_alert
+from telegram_bot import send_alert, send_welcome
 from utils import check_retraction_signal
 
 CHECK_INTERVAL = 180  # 3 minutos
@@ -18,6 +16,10 @@ async def monitor():
                     await send_alert(result)
         await asyncio.sleep(CHECK_INTERVAL)
 
-if __name__ == "__main__":
+async def main():
+    await send_welcome()  # envia a mensagem de start no Telegram
     print("✅ Bot iniciado e monitorando sinais...")
-    asyncio.run(monitor())
+    await monitor()
+
+if __name__ == "__main__":
+    asyncio.run(main())
