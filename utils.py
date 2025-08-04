900eb77751a8dcfde32c8a2d7ef7f0ef61628b4b
telegrambet <guilhermer6futebol@gmail.com>
2025-08-04 09:49:06 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,8 +4,10 @@ PAIRS = {
     'frxEURJPY': 35.1,
     'frxGBPJPY': 35.5,
     'frxCADJPY': 35.9,
-    'frxAUDCAD': 35.2,
+    'frxAUDJPY': 35.2,
     'frxEURGBP': 35.3,
+    'frxEURUSD': 35.2,
+    'frxUSDCAD': 35.2,
 }
 
 def calculate_pips(open_price, close_price):
